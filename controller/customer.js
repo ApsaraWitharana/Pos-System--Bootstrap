10f38b855b399114c422eeefbe720a83a5ff8ec1
ApsaraWitharana <asachini095@gmail.com>
2024-05-22 19:01:53 +0530

    update

--- controller/customer.js
+++ controller/customer.js
@@ -117,3 +117,16 @@ function loadAllCustomerId() {
     }
 }
 
+// export var loadTable=[{
+//      id : "C00-001",
+//     name:"Sachini Apsara",
+//     address:"Matara",
+//     salary:10000.00,
+// },
+// {
+//     id : "C00-002",
+//     name:"Duvindu Thathsara",
+//     address:"Galle",
+//     salary:20000.00,
+// }
+// ]
--- db/db.js
+++ db/db.js
@@ -1,4 +1,17 @@
-export var customer=[];
+export var customer=[{
+    id : "C00-001",
+    name:"Sachini Apsara",
+    address:"Matara",
+    salary:10000.00,
+},
+{
+    id : "C00-002",
+    name:"Duvindu Thathsara",
+    address:"Galle",
+    salary:20000.00,
+}
+    
+];
 export var items=[];
 export var orderDetails=[];
 export var order=[];
