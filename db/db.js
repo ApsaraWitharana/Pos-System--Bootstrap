df1d15ba1becf98dc885593efc67e464eca996f5
ApsaraWitharana <asachini095@gmail.com>
2024-05-23 21:12:09 +0530

    add items to array

--- db/db.js
+++ db/db.js
@@ -12,6 +12,19 @@ export var customer=[{
 }
     
 ];
-export var items=[];
+export var items=[{
+    id:"I00-001",
+    name: "Punnisamba",
+    price:250.00,
+    qty:10
+
+},
+{
+    id:"I00-002",
+    name: "Keerisamba",
+    price:300.00,
+    qty:10
+
+}];
 export var orderDetails=[];
 export var order=[];
